4178d05b1e673f5004451c963d5c02b4cecbb42e
Iden Dodd <theguruofreason@example.com>
2013-12-28 19:47:38 -0800

    added a basic main that creates a window

--- main.hs
+++ main.hs
@@ -1,8 +1,27 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
-import           Graphics.UI.GLFW
+import           Control.Lens
+import           Control.Monad                 (when)
+import qualified Graphics.Rendering.OpenGL.Raw as GL
+import qualified Graphics.UI.GLFW              as GLFW
 import           Reactive.Banana
-import Control.Lens
+import           System.IO
 
+main :: IO ()
 main = do
-  
+  GLFW.init
+  GLFW.setTime 0
+  GLFW.createWindow 640 480 "GLFW-b Test" Nothing Nothing
+  return ()
+
+{-let networkDescription :: forall t Frameworks t = Moment t ()
+    networkDescription = do
+      -- input : obtain Event from functions that register event
+      -- handlers
+      emouse    <- fromAddHandler $ registerMouseEvent window
+      ekeyboard <- fromAddHandler $ registerKeyEvent window
+      -- input : obtain Behavior from mutable data by polling
+      bdie      <- fromPoll       $ randomRIO (1,6)
+
+      -- express event graph
+-}
