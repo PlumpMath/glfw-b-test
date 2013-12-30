551edf9eefc10ac4523807a4cacc6215daa353ac
Iden Dodd <theguruofreason@example.com>
2013-12-29 17:15:00 -0800

    minor cleanup

--- main.hs
+++ main.hs
@@ -3,7 +3,7 @@ module Main where
 import           Control.Lens
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW          as GLFW
-import           Reactive.Banana
+import Reactive.Banana.Frameworks
 import           System.IO
 
 main :: IO ()
@@ -11,15 +11,12 @@ main = do
   initSuccess <- GLFW.init
   GLFW.setTime 0
   window <- GLFW.createWindow 640 480 "GLFW-b Test" Nothing Nothing
-
-  let testNetwork :: forall t. Frameworks t => Moment t ()
-      testNetwork = do
-         -- input : obtain Event from functions that register event
-         -- handlers
-         emouse    <- fromAddHandler $ setKeyCallback window
-         ekeyboard <- fromAddHandler $ setMouseCallback window
-      reactimate $ fmap print emouse
-      reactimate $ fmap print ekeyboard
-
+  let testNetwork = do
+        -- input : obtain Event from functions that register event
+        -- handlers
+        emouse    <- fromAddHandler $ GLFW.setKeyCallback window
+        ekeyboard <- fromAddHandler $ GLFW.setMouseButtonCallback window
+        reactimate $ fmap print emouse
+        reactimate $ fmap print ekeyboard
   network <- compile testNetwork
   actuate network
