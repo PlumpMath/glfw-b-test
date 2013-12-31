8f24db6e54c43b6670bc5dd4af74135e141732a2
Chong Li <chongli@gmail.com>
2013-12-31 00:03:38 -0500

    Added random colours on every event.

--- Main.hs
+++ Main.hs
@@ -1,15 +1,30 @@
-{-# LANGUAGE RankNTypes      #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Main where
 import           Control.Applicative
 --import           Control.Lens
 import           Control.Monad              (forever, when)
---import qualified Graphics.Rendering.OpenGL  as GL
+import           Foreign.C.Types
 import           Game.Handlers
+import           Graphics.Rendering.OpenGL  (($=))
+import qualified Graphics.Rendering.OpenGL  as GL
 import qualified Graphics.UI.GLFW           as GLFW
 import           Reactive.Banana
 import           Reactive.Banana.Frameworks
 import           System.Exit                (exitSuccess)
+import           System.Random
+
+newtype MyColor = MyColor { unMyColor :: GL.Color4 CFloat }
+
+instance Random MyColor where
+  randomR (MyColor (GL.Color4 rl gl bl al), MyColor (GL.Color4 rh gh bh ah)) gen = let
+    (r,gen2) = randomR (rl,rh) gen
+    (g,gen3) = randomR (gl,gh) gen2
+    (b,gen4) = randomR (bl,bh) gen3
+    (a,gen5) = randomR (al,ah) gen4
+    in (MyColor (GL.Color4 r g b a),gen5)
+  random gen = randomR (MyColor (GL.Color4 0.0 0.0 0.0 0.0), MyColor (GL.Color4 1.0 1.0 1.0 1.0)) gen
 
 main :: IO ()
 main = do
@@ -29,10 +44,12 @@ mainLoop win = do
         -- handlers
         eKeyChar <- fromAddHandler $ charHandler win
         eMouseButton <- fromAddHandler $ mouseButtonHandler win
+        eRandomColor <- changes =<< fromPoll (unMyColor <$> randomIO)
         let
             eCharAccum = accumB [] $ (:) . snd <$> eKeyChar
         keyList <- changes eCharAccum
         eKey <- fromAddHandler $ keyHandler win
+        reactimate $ (GL.clearColor $=) <$> eRandomColor
         reactimate $ const exitSuccess <$> eMouseButton
         reactimate $ (\(_,c) -> if c == 'q' then exitSuccess else print c) <$> eKeyChar
         reactimate $ print <$> keyList
@@ -40,5 +57,11 @@ mainLoop win = do
   network <- compile testNetwork
   actuate network
   putStrLn "Click the mouse button inside the window to exit!"
-  forever GLFW.waitEvents
+  GLFW.makeContextCurrent $ Just win
+  forever (draw >> GLFW.swapBuffers win >> GLFW.waitEvents)
+
+draw :: IO ()
+draw = do
+  GL.clear [GL.ColorBuffer]
+  GL.flush
 
