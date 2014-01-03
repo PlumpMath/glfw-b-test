6bfd55b0e24e971de126f9291f39b374db14c11e
Chong Li <chongli@gmail.com>
2014-01-03 05:07:50 -0500

    Gutted Main.hs, making way for netwire transition.

--- Main.hs
+++ Main.hs
@@ -2,68 +2,39 @@
 {-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE TemplateHaskell   #-}
 module Main where
-import           Control.Applicative
 --import           Control.Lens
-import           Control.Monad
-import qualified Data.ByteString            as BS
-import           Foreign.C.Types
+import           qualified Control.Monad as M
 import           Foreign.Ptr
+import           FRP.Netwire
 import           Game.Handlers
 import           Graphics.GLUtil
-import           Graphics.Rendering.OpenGL  (($=))
-import qualified Graphics.Rendering.OpenGL  as GL
-import qualified Graphics.UI.GLFW           as GLFW
+import           Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLFW          as GLFW
+import Prelude hiding ((.),id)
 import           Paths_glfw_b_test
-import           Reactive.Banana
-import           Reactive.Banana.Frameworks
-import           System.Exit                (exitSuccess)
+import           System.Exit               (exitSuccess)
 import           System.Random
 
-newtype MyColor = MyColor { unMyColor :: GL.Color4 CFloat }
-
-instance Random MyColor where
-  randomR (MyColor (GL.Color4 rl gl bl al), MyColor (GL.Color4 rh gh bh ah)) gen = let
-    (r,gen2) = randomR (rl,rh) gen
-    (g,gen3) = randomR (gl,gh) gen2
-    (b,gen4) = randomR (bl,bh) gen3
-    (a,gen5) = randomR (al,ah) gen4
-    in (MyColor (GL.Color4 r g b a),gen5)
-  random gen = randomR (MyColor (GL.Color4 0.0 0.0 0.0 0.0), MyColor (GL.Color4 1.0 1.0 1.0 1.0)) gen
-
 main :: IO ()
-main = do
-  initSuccess <- GLFW.init
-  GLFW.setTime 0
-  when initSuccess $ do
-    window <- GLFW.createWindow 640 480 "GLFW-b Test" Nothing Nothing
-    case window of
-      Just win -> mainLoop win
-      Nothing  -> return ()
-
-mainLoop :: GLFW.Window -> IO ()
-mainLoop win = do
-  let testNetwork :: forall t. Frameworks t => Moment t ()
-      testNetwork = do
-        -- input : obtain Event from functions that register event
-        -- handlers
-        eKeyChar <- fromAddHandler $ charHandler win
-        eMouseButton <- fromAddHandler $ mouseButtonHandler win
-        eRandomColor <- changes =<< fromPoll (unMyColor <$> randomIO)
-        let
-            eCharAccum = accumB [] $ (:) . snd <$> eKeyChar
-        keyList <- changes eCharAccum
-        eKey <- fromAddHandler $ keyHandler win
-        --reactimate $ (GL.clearColor $=) <$> eRandomColor
-        reactimate $ const exitSuccess <$> eMouseButton
-        reactimate $ (\(_,c) -> if c == 'q' then exitSuccess else print c) <$> eKeyChar
-        --reactimate $ print <$> keyList
-        --reactimate $ print <$> eKey
-  network <- compile testNetwork
-  actuate network
-  GLFW.makeContextCurrent $ Just win
+main = withInitGLFW $ \win -> do
   prog <- initializeOpenGL
   GL.currentProgram $= Just (program prog)
-  forever (draw >> GLFW.swapBuffers win >> GLFW.waitEvents)
+  GLFW.setKeyCallback win (Just kcb)
+  M.forever $ do
+    parseEvents
+    draw
+    GLFW.swapBuffers win
+  where
+    kcb _ _ _ _ _ = putStrLn "kcb!"
+    parseEvents = do
+      GLFW.pollEvents
+
+withInitGLFW :: (GLFW.Window -> IO ()) -> IO ()
+withInitGLFW act = GLFW.init >>= \i -> M.when i $ do
+  win <- GLFW.createWindow 640 480 "GLFW-b Test" Nothing Nothing
+  GLFW.makeContextCurrent win
+  maybe (return ()) act win
 
 initializeOpenGL :: IO ShaderProgram
 initializeOpenGL = do
